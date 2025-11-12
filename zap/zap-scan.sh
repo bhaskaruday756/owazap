e5ceddc003f76e3549ad86325890eb5ea8136f49
bhaskaruday756 <91750505+bhaskaruday756@users.noreply.github.com>
2025-11-12 18:46:36 +0530

    Create zap-scan.sh

---
+++ zap/zap-scan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+TARGET_URL=${1:-"https://yourapp.example.com"}
+ZAP_PATH="./zap_home"
+
+echo "[*] Starting Passive Scan..."
+$ZAP_PATH/zap.sh -cmd -quickurl $TARGET_URL -quickout baseline-report.html -addonupdate -quickprogress -configfile zap/zap-baseline.conf
+
+echo "[*] Starting Active Scan..."
+$ZAP_PATH/zap.sh -cmd -quickurl $TARGET_URL -quickout fullscan-report.html -addonupdate -quickprogress -quickscan -configfile zap/zap-fullscan.conf
+
+echo "[*] Reports generated:"
+ls -l *.html
